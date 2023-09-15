c91b349e4941b4ea994222623c6b4c5e081de13e
Ben Hoberg <benjhoberg@gmail.com>
2023-09-15 12:09:32 -0500

    Removed unused import statement from Watchlist.js

--- src/components/Titles/TitleEdit.js
+++ src/components/Titles/TitleEdit.js
@@ -13,7 +13,7 @@ export const TitleEdit = () => {
     })
 
     const myHeaders = new Headers();
-    myHeaders.append("X-RapidAPI-Key", "692a3bc309msh31d29e11c582aa5p1aa1c6jsn45689d696937");
+    myHeaders.append("X-RapidAPI-Key", "814b065451msh799ec8d3ff2240ap1684a7jsnc2d335e70579");
     myHeaders.append("X-RapidAPI-Host", "unogs-unogs-v1.p.rapidapi.com");
 
     const requestOptions = {
